b4670658ba036ab90cad931cce7d9a7d87a24dfc
K4rleak <49683415+K4rleak@users.noreply.github.com>
2023-11-23 11:57:56 +0100

    Add files via upload

---
+++ Zkouska_nanecisto/Book.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include<iostream>
+#include<sstream>
+
+using namespace std;
+
+class Book{
+private:
+
+long id;
+string name;
+string author;
+bool available;
+
+public:
+
+Book(string book);
+void setID(long nid);
+
+bool operator < (const Book& obj);
+bool operator ==(const Book& obj);
+
+friend ostream& operator<<(std::ostream& out, const Book& obj);
+friend class Library;
+};
